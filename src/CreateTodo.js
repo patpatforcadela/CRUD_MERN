581883f2ec533aa92cf27a16f94dd02c246b9544
Patrick <ptrckxforcadela@gmail.com>
2021-08-16 12:55:01 +0800

    Fix error in CreateTodo.js

--- src/CreateTodo.js
+++ src/CreateTodo.js
@@ -3,13 +3,12 @@ import { TodoForm } from './TodoForm';
 export const CreateTodo = () => {
     const onSubmit = (data) => {
         alert(JSON.stringify(data));
-        history.push('/')
     }
     return (
         <div className="container">
             <div className="mt-3">
                 <h3>Create Todo Item</h3>
-                <TodoForm todo={todo} onSubmit={onSubmit} />
+                <TodoForm onSubmit={onSubmit} />
             </div>
         </div>
     )
